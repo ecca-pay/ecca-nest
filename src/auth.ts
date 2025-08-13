36d1d1041d83513f6d89bbb0aaacc3efabf1beda
Oleh Mikadze <oleg.mikadze@completecase.com>
2025-08-13 14:25:16 +0200

    additional fixes (migration scripts, auth for endpoints)

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../generated/prisma';
+import { organization } from 'better-auth/plugins';
 
 const prisma = new PrismaClient();
 
@@ -8,4 +9,18 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
   }),
+  trustedOrigins: ['http://localhost:5173'],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [organization()],
+  // user: {
+  //   additionalFields: {
+  //     roles: {
+  //       type: [Role.ADMIN, Role.TRADER, Role.MEMBER],
+  //       required: true,
+  //       defaultValue: [Role.MEMBER],
+  //     },
+  //   },
+  // },
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,34 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
-import { AuthGuard, Session, UserSession } from '@thallesp/nestjs-better-auth';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard, AuthService, Session } from '@thallesp/nestjs-better-auth';
+import { Public } from '@thallesp/nestjs-better-auth';
+import type { Request as ExpressRequest } from 'express';
+import { auth } from 'src/auth';
+import { fromNodeHeaders } from 'better-auth/node';
+import type { UserSession } from '@thallesp/nestjs-better-auth';
 
 @Controller('users')
-@UseGuards(AuthGuard)
+// @UseGuards(AuthGuard) // Apply to all routes in this controller
 export class UserController {
-  //   @Get('me')
-  //    getProfile(@Session() session: UserSession) {
-  //     return { user: session.user };
-  //   }
+  constructor(private authService: AuthService<typeof auth>) {}
+
+  @Get('me')
+  getProfile(@Session() session: UserSession) {
+    return session;
+  }
+
+  @Get('public')
+  @Public() // Mark this route as public (no authentication required)
+  publicRoute() {
+    return { message: 'This route is public' };
+  }
+
+  @Get('accounts')
+  async getAccounts(@Request() req: ExpressRequest) {
+    // Pass the request headers to the auth API
+    const accounts = await this.authService.api.listUserAccounts({
+      headers: fromNodeHeaders(req.headers),
+    });
+
+    return { accounts };
+  }
 }
